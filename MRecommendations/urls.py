2b332b3d726083eca03a2e4cd13e22db30cffdd1
iam-monu <69475573+iam-monu@users.noreply.github.com>
2021-09-24 23:28:48 +0530

    updated
    added 
    from django.conf import settings
    from django.conf.urls.static import static

--- MRecommendations/urls.py
+++ MRecommendations/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.site.site_header = "Movie Recommendation!"
 admin.site.site_title = "Movie Admin Portal"
 admin.site.index_title = "Welcome to Movie Reccomender"
