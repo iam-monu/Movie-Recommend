9e43adb43f69d384df2eaa5faba48b93b2c78fb4
iam-monu <69475573+iam-monu@users.noreply.github.com>
2021-09-24 23:30:25 +0530

    added STATIC_ROOT and removed STATICFILES_DIRS
    STATIC_ROOT =os.path.join(BASE_DIR,'static')

--- MRecommendations/settings.py
+++ MRecommendations/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #added manually
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS =(
+#     os.path.join(BASE_DIR, "static"),
+# )
+
+STATIC_ROOT =os.path.join(BASE_DIR,'static')
